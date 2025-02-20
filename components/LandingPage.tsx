13a2d21a18577a864b8e766f7e2ca937eee3073a
Darren Teo HH <darrenteo77@gmail.com>
2025-02-21 02:06:51 +0800

    Continue work on landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import '../styles/global.css';
 
 export default function LandingPage() {
   return (
     <div className="w-full bg-[url('/images/land.png')] min-h-screen bg-center bg-cover"> 
-        <nav className="flex items-center">
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent via-90% to-white"></div>
+      <nav className="flex items-center">
             <img className ="w-32 h-32 "src = "/images/whiteimage.png"></img>
             <ul className="flex-auto text-center">
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">Home</a></li>
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">About</a></li>                
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">Contact</a></li>            
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Home</a></li>
+                <li className= "list-none inline-block px-4"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">About</a></li>                
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Contact</a></li>            
             </ul>  
         </nav> 
+        <div>
+            <h1 className="text-6xl font-semibold text-white">welcome</h1>
+            <p>Test</p>
+        </div>
     </div>
   );
 }
