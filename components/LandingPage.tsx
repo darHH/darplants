8bd16a73f555e4461e24c7bebf0d46e7959b2cd9
Darren Teo HH <darrenteo77@gmail.com>
2025-02-20 22:35:30 +0800

    Continue work on landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 export default function LandingPage() {
   return (
-    <div className="container bg-[url('/images/land.png')] bg-contain bg-no-repeat"> 
-        <nav className="">
-            <img src = "/images/whiteimage.png"></img>
-            <ul className="">
+    <div className="w-full bg-[url('/images/land.png')] min-h-screen bg-center bg-cover"> 
+        <nav className="flex items-center">
+            <img className ="w-32 h-32 "src = "/images/whiteimage.png"></img>
+            <ul className="flex-auto text-center">
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">Home</a></li>
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">About</a></li>                
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4">Contact</a></li>            
             </ul>  
         </nav> 
     </div>
