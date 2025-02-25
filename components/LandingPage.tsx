04f63f53a976a2fd20b1804d7d8d129439f16b3e
Darren Teo HH <darrenteo77@gmail.com>
2025-02-25 11:34:34 +0800

    Work on landing page - logo icon with scaling, added icons

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -6,17 +6,13 @@ export default function LandingPage() {
     <div className="w-full bg-[url('/images/bonsaiCover2.png')] min-h-screen bg-center bg-cover"> 
       <div className="absolute inset-0"></div>
       <nav className="flex items-center">
-            <img className ="w-32 h-32 "src = "/images/whiteimage.png"></img>
+            <img className ="m-12 w-20 h-20 md:w-28 md:h-28 lg:w-36 lg:h-36 "src = "/images/oglogobig.png"></img>
             <ul className="flex-auto text-center">
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Home</a></li>
-                <li className= "list-none inline-block px-4"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">About</a></li>                
+                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">About</a></li>
+                <li className= "list-none inline-block px-4"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Plants</a></li>                
                 <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Contact</a></li>            
             </ul>  
         </nav> 
-        <div>
-            <h1 className="text-6xl font-semibold text-white">welcome</h1>
-            <p>Test</p>
-        </div>
     </div>
   );
 }
