8055b69bc5008849754caa377e02e2cd74811f21
Darren Teo HH <darrenteo77@gmail.com>
2025-02-25 12:26:56 +0800

    Add basic AboutMe component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import InstagramFeed from '../components/InstagramFeed';
 import LandingPage from '../components/LandingPage';
+import AboutMe from '../components/AboutMe';
 
 export default function Home() {
   return (
     <>
       <LandingPage />
-      {/* about me */}
+      <AboutMe />
       <InstagramFeed />
       {/* more to come... contact me! */}
     </>
--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-import "../styles/global.css";
+import React from 'react';
+import '../styles/global.css';
 
 export default function AboutMe() {
-    
+    return (
+        
+        <h1>test</h1>
+
+    );
 }
