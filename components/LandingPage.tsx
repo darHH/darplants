183d2339a2e815f784d803fe1ac48ced1e95e3a1
Darren Teo HH <darrenteo77@gmail.com>
2025-02-25 12:14:44 +0800

    Work on landing page - add profile icon

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -6,12 +6,25 @@ export default function LandingPage() {
     <div className="w-full bg-[url('/images/bonsaiCover2.png')] min-h-screen bg-center bg-cover"> 
       <div className="absolute inset-0"></div>
       <nav className="flex items-center">
-            <img className ="m-12 w-20 h-20 md:w-28 md:h-28 lg:w-36 lg:h-36 "src = "/images/oglogobig.png"></img>
+            <img className="m-12 w-20 h-20 md:w-28 md:h-28 lg:w-36 lg:h-36 drop-shadow-lg" src="/images/oglogobig.png"></img>
             <ul className="flex-auto text-center">
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">About</a></li>
-                <li className= "list-none inline-block px-4"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Plants</a></li>                
-                <li className= "list-none inline-block px-4>"><a href="#" className="no-underline text-white px-4 font-nunito font-medium">Contact</a></li>            
+                <li className= "list-none inline-block px-6>">
+                  <a href="#" className="no-underline text-white text-lg px-6 font-nunito font-semibold">
+                    About
+                  </a>
+                </li>
+                <li className= "list-none inline-block px-6">
+                  <a href="#" className="no-underline text-white text-lg px-6 font-nunito font-semibold">
+                    Plants
+                  </a>
+                </li>                
+                <li className= "list-none inline-block px-6>">
+                  <a href="#" className="no-underline text-white text-lg px-6 font-nunito font-semibold">
+                    Contact
+                  </a>
+                </li>            
             </ul>  
+            <img className="mr-16 w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" src="/images/robot.png"></img>
         </nav> 
     </div>
   );
