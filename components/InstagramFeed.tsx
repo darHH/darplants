69787218ecb79c0feb79985709057a9a8cd65e7d
Darren Teo HH <darrenteo77@gmail.com>
2025-02-27 15:48:09 +0800

    Fix warnings for hosting

--- components/InstagramFeed.tsx
+++ components/InstagramFeed.tsx
@@ -24,7 +24,7 @@ const InstagramFeed: React.FC = () => {
     };
 
     loadPosts();
-  }, []);
+  }, [instagramService]);
 
   const sortedPosts = [...posts].sort((a, b) => {
     if (sortBy === 'price') {
